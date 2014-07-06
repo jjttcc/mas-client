ab82804a29801e25921d2c98cbd340182287cb9f
Jim Cochrane <jim.cochrane@gmail.com>
2014-07-06 06:39:42 -0500

    tradable analyzer abstraction
    New class for encapsulating the tradable analyzers (AKA tradable-event
    generators) concept - currently, just a name and id.  The id is used in
    'request_analysis' (described in the previous commit [for
    MasCommunicationServices]) to tell the server which analyzer(s) to use.

---
+++ tradable_analyzer.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'ruby_contracts'
+
+
+# Client-side representation of objects on the server side responsible for
+# analyzing tradable data and resulting producing signal output
+class TradableAnalyzer
+  include Contracts::DSL
+
+  public
+
+  attr_reader :name, :id
+
+  private
+
+  attr_reader :socket
+
+  def initialize(name, id)
+    @name = name
+    @id = id
+  end
+
+end

jjttcc/market-analysis-system

commit 549617d12862b60d0ae611c9adcfee6673a9ce47

First attempt to implement end-date for analysis
Added setting and use of 'end_date_time' for analysis.  Although
EVENT_DATA_REQUEST_CMD has an 'analysis_end_date' attribute, it
(unlike 'analysis_start_date') was not being used for the analysis.
An 'end_date_time' attribute was added to some classes.  And
'exhausted' was redefined in ONE_VARIABLE_LINEAR_ANALYZER and
TWO_VARIABLE_FUNCTION_ANALYZER to return true if target*.exhausted
(Precursor) OR target*.item.date_time > end_date_time.  This
implementation is inadequate.  For proper back-testing, the end-date
will need to serve as a cut-off date for the original data - i.e.,
of the stock or commodity being processed (to simulate actually
running the analysis at that date, in the past).  This implementation
only applies the end-date to the top-level function or analyzer,
which allows "future" signals to sometimes leak into the results.

Also committing an unrelated fix in MAS_SESSION.update_parameter:
removed: "params_match: dest_param.verbose_name ~ src_param.verbose_name"
This precondition is no longer valid, with the enforce-unique-parameter-
names fix.

src/library/event_generation/event_generation_kernel/compound_event_generator.e
@@ -119,6 +119,14 @@ feature -- Status setting
             right_analyzer.set_start_date_time (d)
         end
 
+    set_end_date_time (d: DATE_TIME)
+            -- Set `left_analyzer' and `right_analyzer' `end_date_time's
+            -- to `d'.
+        do
+            left_analyzer.set_end_date_time (d)
+            right_analyzer.set_end_date_time (d)
+        end
+
     set_before_extension (arg: DATE_TIME_DURATION)
             -- Set before_extension to -`arg'.
         require

src/library/event_generation/event_generation_kernel/market_event_coordinator.e
@@ -1,84 +1,105 @@
 note
-	description:
-		"An event coordinator that uses tradable event generators to generate %
-		%tradable events and passes a queue of the generated events to a %
-		%dispatcher"
-	author: "Jim Cochrane"
-	date: "$Date$";
-	revision: "$Revision$"
+    description:
+        "An event coordinator that uses tradable event generators to generate %
+        %tradable events and passes a queue of the generated events to a %
+        %dispatcher"
+    author: "Jim Cochrane"
+    date: "$Date$";
+    revision: "$Revision$"
     copyright: "Copyright (c) 1998-2014, Jim Cochrane"
     license:   "GPL version 2 - http://www.gnu.org/licenses/gpl-2.0.html"
+    -- settings: vim: expandtab
 
 deferred class MARKET_EVENT_COORDINATOR inherit
 
-	EVENT_COORDINATOR
-		redefine
-			event_generators
-		end
+    EVENT_COORDINATOR
+        redefine
+            event_generators
+        end
 
 feature -- Access
 
-	event_generators: LINEAR [TRADABLE_EVENT_GENERATOR]
+    event_generators: LINEAR [TRADABLE_EVENT_GENERATOR]
 
-	start_date_time: DATE_TIME
-			-- Date and time that the generators are to begin their
-			-- analysis - that is, for each tradable, only the tradable's
-			-- data whose date/time >= `start_date_time' will be processed.
+    start_date_time: DATE_TIME
+            -- Date and time that the generators are to begin their
+            -- analysis - that is, for each tradable, only the tradable's
+            -- data whose date/time >= `start_date_time'
+            -- (and whose date/time <= `end_date_time') will be processed.
+
+    end_date_time: DATE_TIME
+            -- Date and time at which the generators are to end their
+            -- analysis - that is, for each tradable, only the tradable's
+            -- data whose date/time <= `end_date_time'
+            -- (and whose date/time >= `start_date_time') will be processed.
 
 feature -- Status setting
 
-	set_start_date_time (d: DATE_TIME)
-			-- For each member of `event_generators', set the date and time
-			-- for which to begin analysis to `d' and update `start_date_time'
-			-- to `d'.
-		do
-			start_date_time := d
-			update_generators_date_time
-		ensure
-			date_time_set: start_date_time = d
-		end
+    set_start_date_time (d: DATE_TIME)
+            -- For each member of `event_generators', set the date and time
+            -- for which to begin analysis to `d' and update `start_date_time'
+            -- to `d'.
+        do
+            start_date_time := d
+            update_generators_date_time
+        ensure
+            date_time_set: start_date_time = d
+        end
 
-	set_event_generators (arg: LINEAR [TRADABLE_EVENT_GENERATOR])
-			-- Set event_generators to `arg', call `set_start_date_time'
-			-- on each one with `start_date_time', and do any other
-			-- initialization needed on each event generator.
-		require
-			arg_not_void: arg /= Void
-			date_not_void: start_date_time /= Void
-		do
-			event_generators := arg
-			update_generators_date_time
-		ensure
-			event_generators_set: event_generators = arg and
-				event_generators /= Void
-		end
+    set_end_date_time (d: DATE_TIME)
+            -- For each member of `event_generators', set the date and time
+            -- for which to end analysis to `d' and update `end_date_time'
+            -- to `d'.
+        do
+            end_date_time := d
+            update_generators_date_time
+        ensure
+            date_time_set: end_date_time = d
+        end
 
-	set_dispatcher (arg: EVENT_DISPATCHER)
-			-- Set dispatcher to `arg'.
-		require
-			arg_not_void: arg /= Void
-		do
-			dispatcher := arg
-		ensure
-			dispatcher_set: dispatcher = arg and dispatcher /= Void
-		end
+    set_event_generators (arg: LINEAR [TRADABLE_EVENT_GENERATOR])
+            -- Set event_generators to `arg', call
+            -- `set_start_date_time'/`set_end_date_time' on each one with
+            -- `start_date_time'/`end_date_time', and do any other
+            -- initialization needed on each event generator.
+        require
+            arg_not_void: arg /= Void
+            dates_not_void: start_date_time /= Void and end_date_time /= Void
+        do
+            event_generators := arg
+            update_generators_date_time
+        ensure
+            event_generators_set: event_generators = arg and
+                event_generators /= Void
+        end
+
+    set_dispatcher (arg: EVENT_DISPATCHER)
+            -- Set dispatcher to `arg'.
+        require
+            arg_not_void: arg /= Void
+        do
+            dispatcher := arg
+        ensure
+            dispatcher_set: dispatcher = arg and dispatcher /= Void
+        end
 
 feature {NONE} -- Implementation
 
-	update_generators_date_time
-			-- Call `set_start_date_time' on each generator with
-			-- `start_date_time'.
-		do
-			if event_generators /= Void then
-				from
-					event_generators.start
-				until
-					event_generators.exhausted
-				loop
-					event_generators.item.set_start_date_time (start_date_time)
-					event_generators.forth
-				end
-			end
-		end
+    update_generators_date_time
+            -- Call `set_start_date_time'/`set_end_date_time' on each
+            -- generator with `start_date_time'/`end_date_time'.
+        do
+            if event_generators /= Void then
+                from
+                    event_generators.start
+                until
+                    event_generators.exhausted
+                loop
+                    event_generators.item.set_start_date_time (start_date_time)
+                    event_generators.item.set_end_date_time (end_date_time)
+                    event_generators.forth
+                end
+            end
+        end
 
 end -- class MARKET_EVENT_COORDINATOR

src/library/event_generation/event_generation_kernel/tradable_event_generator.e
@@ -151,6 +151,13 @@ feature -- Status setting
         deferred
         end
 
+    set_end_date_time (d: DATE_TIME)
+            -- Set the date and time to end the analysis to `d'.
+        require
+            not_void: d /= Void
+        deferred
+        end
+
 feature {TRADABLE_FUNCTION_EDITOR}
 
     wipe_out

src/library/event_generation/function_analysis/function_analyzer.e
@@ -23,6 +23,10 @@ feature -- Access
 			-- Date/time specifying which trading period to begin the
 			-- analysis of tradable data
 
+	end_date_time: DATE_TIME
+			-- Date/time specifying which trading period to end the
+			-- analysis of tradable data
+
 	operator: RESULT_COMMAND [BOOLEAN]
 			-- Operator used to analyze each tuple - evaluation to True
 			-- will result in an event being generated.
@@ -56,6 +60,14 @@ feature -- Status setting
 		ensure then
 			start_date_time_set: start_date_time = d and
 									start_date_time /= Void
+		end
+
+	set_end_date_time (d: DATE_TIME)
+		do
+			end_date_time := d
+		ensure then
+			end_date_time_set: end_date_time = d and
+									end_date_time /= Void
 		end
 
 	set_operator (arg: RESULT_COMMAND [BOOLEAN])

src/library/event_generation/function_analysis/function_based_command.e
@@ -1,209 +1,214 @@
 note
-	description: "A linear command that gets its input from a TRADABLE_FUNCTION"
-	note1: "@@This class has recently been modified to be used for indicators %
-		%in addition to its original use by ONE_VARIABLE_FUNCTION_ANALYZER.%
-		%Although this new functionality has been tested successfully, it %
-		%has not yet been tested thoroughly."
-	author: "Jim Cochrane"
-	date: "$Date$";
-	revision: "$Revision$"
+    description: "A linear command that gets its input from a TRADABLE_FUNCTION"
+    note1: "@@This class has recently been modified to be used for indicators %
+        %in addition to its original use by ONE_VARIABLE_FUNCTION_ANALYZER.%
+        %Although this new functionality has been tested successfully, it %
+        %has not yet been tested thoroughly."
+    author: "Jim Cochrane"
+    date: "$Date$";
+    revision: "$Revision$"
     copyright: "Copyright (c) 1998-2014, Jim Cochrane"
     license:   "GPL version 2 - http://www.gnu.org/licenses/gpl-2.0.html"
+    -- settings: vim: expandtab
 
 class FUNCTION_BASED_COMMAND inherit
 
-	UNARY_LINEAR_OPERATOR
-		rename
-			make as ulo_make_unused
-		export
-			{NONE} ulo_make_unused
-		undefine
-			start
-		redefine
-			initialize, execute, target_cursor_not_affected,
-			prepare_for_editing, is_editable
-		end
-	
-	ONE_VARIABLE_LINEAR_ANALYZER
+    UNARY_LINEAR_OPERATOR
+        rename
+            make as ulo_make_unused
+        export
+            {NONE} ulo_make_unused
+        undefine
+            start,
+            exhausted   --!!!!!<end_date_time change> !!![check!!!]
+        redefine
+            initialize, execute, target_cursor_not_affected,
+            prepare_for_editing, is_editable
+        end
+    
+    ONE_VARIABLE_LINEAR_ANALYZER
 
-	TRADABLE_FUNCTION_EDITOR
+    TRADABLE_FUNCTION_EDITOR
 
 creation
 
-	make
+    make
 
 feature {NONE} -- Initialization
 
-	make (in: like input; op: like operand)
-		require
-			not_void: in /= Void and op /= Void
-		do
-			input := in
-			set (input.output)
-			set_operand (op)
-		ensure
-			set: input = in and operand = op
-		end
+    make (in: like input; op: like operand)
+        require
+            not_void: in /= Void and op /= Void
+        do
+            input := in
+            set (input.output)
+            set_operand (op)
+        ensure
+            set: input = in and operand = op
+        end
 
 feature -- Initialization
 
-	initialize (a: LINEAR_ANALYZER)
-		local
-			analyzer: ONE_VARIABLE_FUNCTION_ANALYZER
-			function: TRADABLE_FUNCTION
-		do
-			analyzer ?= a
-			if analyzer /= Void then
-				start_date_time := analyzer.start_date_time
-				left_offset := analyzer.left_offset
-				innermost_input := analyzer.current_tradable
-			else
-				function ?= a
-				check
-					arg_is_a_function: function /= Void
-				end
-				if function.innermost_input.count > 0 then
-					start_date_time :=
-						function.innermost_input.first.date_time
-				end
-				left_offset := 0
-				innermost_input := function.innermost_input
-				function_based := True
-			end
-			input.set_innermost_input (innermost_input)
-			if not input.processed then
-				input.process
-			end
-			-- Advance target cursor according to start_date_time and
-			-- left_offset.
-			start
-			operand.initialize (Current)
-			if
-				function /= Void and then
-				not target.is_empty and then
-				start_date_time < target.item.date_time
-			then
-				-- Ensure the postcondition when `a' is a TRADABLE_FUNCTION.
-				start_date_time := target.item.date_time
-			end
-		ensure then
-			input_processed: input.processed
-			date_not_earlier: not target.exhausted implies
-							target.item.date_time >= start_date_time
-			offset_constraint:
-				not target.exhausted implies target.index >= left_offset
-			innermost_input_exists: input.processed implies
-				innermost_input /= Void
-		end
+    initialize (a: LINEAR_ANALYZER)
+        local
+            analyzer: ONE_VARIABLE_FUNCTION_ANALYZER
+            function: TRADABLE_FUNCTION
+        do
+            analyzer ?= a
+            if analyzer /= Void then
+                start_date_time := analyzer.start_date_time
+                end_date_time := analyzer.end_date_time
+                left_offset := analyzer.left_offset
+                innermost_input := analyzer.current_tradable
+            else
+                function ?= a
+                check
+                    arg_is_a_function: function /= Void
+                end
+                if function.innermost_input.count > 0 then
+                    start_date_time :=
+                        function.innermost_input.first.date_time
+                end
+                left_offset := 0
+                innermost_input := function.innermost_input
+                function_based := True
+            end
+            input.set_innermost_input (innermost_input)
+            if not input.processed then
+                input.process
+            end
+            -- Advance target cursor according to start_date_time and
+            -- left_offset.
+            start
+            operand.initialize (Current)
+            if
+                function /= Void and then
+                not target.is_empty and then
+                start_date_time < target.item.date_time
+            then
+                -- Ensure the postcondition when `a' is a TRADABLE_FUNCTION.
+                start_date_time := target.item.date_time
+            end
+        ensure then
+            input_processed: input.processed
+            date_not_earlier: not target.exhausted implies
+                            target.item.date_time >= start_date_time
+            offset_constraint:
+                not target.exhausted implies target.index >= left_offset
+            innermost_input_exists: input.processed implies
+                innermost_input /= Void
+        end
 
 feature -- Access
 
-	input: TRADABLE_FUNCTION
+    input: TRADABLE_FUNCTION
 
-	 start_date_time: DATE_TIME
+    start_date_time: DATE_TIME
+
+    end_date_time: DATE_TIME
 
 feature -- Status report
 
-	target_cursor_not_affected: BOOLEAN
+    target_cursor_not_affected: BOOLEAN
 
-	is_editable: BOOLEAN
-		note
-			once_status: global
-		once
-			Result := True
-		end
+    is_editable: BOOLEAN
+        note
+            once_status: global
+        once
+            Result := True
+        end
 
 feature -- Status setting
 
-	set_input (arg: like input)
-			-- Set input to `arg'.
-		require
-			arg_not_void: arg /= Void
-		do
-			input := arg
-			set (input.output)
-		ensure
-			input_set: input = arg and input /= Void
-		end
+    set_input (arg: like input)
+            -- Set input to `arg'.
+        require
+            arg_not_void: arg /= Void
+        do
+            input := arg
+            set (input.output)
+        ensure
+            input_set: input = arg and input /= Void
+        end
 
 feature -- Basic operations
 
-	execute (arg: ANY)
-		do
-			if function_based then
-				synchronize_target_with_innermost_input
-			end
-			-- The client of this class may have no control over the linear
-			-- structure `target', so responsibility must be taken for
-			-- not incrementing beyond the end of `target'.
-			if not target.off then
-				check
-					dates_match: target.item.date_time.is_equal (
-						input.innermost_input.item.date_time)
-				end
-				operand.execute (target.item)
-				value := operand.value
-				if not function_based then
-					target.forth
-				end
-				target_cursor_not_affected := False
-			else
-				target_cursor_not_affected := not function_based
-			end
-		end
+    execute (arg: ANY)
+        do
+            if function_based then
+                synchronize_target_with_innermost_input
+            end
+            -- The client of this class may have no control over the linear
+            -- structure `target', so responsibility must be taken for
+            -- not incrementing beyond the end of `target'.
+            if not target.off then
+                check
+                    dates_match: target.item.date_time.is_equal (
+                        input.innermost_input.item.date_time)
+                end
+                operand.execute (target.item)
+                value := operand.value
+                if not function_based then
+                    target.forth
+                end
+                target_cursor_not_affected := False
+            else
+                target_cursor_not_affected := not function_based
+            end
+        end
 
-	prepare_for_editing (l: LIST [FUNCTION_PARAMETER])
-		do
-			input.parameters.do_all (agent l.extend)
-		end
+    prepare_for_editing (l: LIST [FUNCTION_PARAMETER])
+        do
+            input.parameters.do_all (agent l.extend)
+        end
 
 feature {NONE} -- Implementation
 
-	function_based: BOOLEAN
-			-- Is the input based on a TRADABLE_FUNCTION (as opposed to a
-			-- ONE_VARIABLE_FUNCTION_ANALYZER?
+    function_based: BOOLEAN
+            -- Is the input based on a TRADABLE_FUNCTION (as opposed to a
+            -- ONE_VARIABLE_FUNCTION_ANALYZER?
 
-	innermost_input: SIMPLE_FUNCTION [TRADABLE_TUPLE]
-			-- Innermost input sequence of `input' - used for efficiency
+    innermost_input: SIMPLE_FUNCTION [TRADABLE_TUPLE]
+            -- Innermost input sequence of `input' - used for efficiency
 
-	synchronize_target_with_innermost_input
-			-- Synchronize `target' with `innermost_input'.
-		require
-			innermost_input_not_off: not innermost_input.off
-		do
-			if target.off then
-				target.start
-			end
-			if
-				not target.off and then target.item.date_time <
-				innermost_input.item.date_time
-			then
-				from
-				until
-					target.exhausted or target.item.date_time.is_equal (
-						innermost_input.item.date_time)
-				loop
-					target.forth
-				end
-			elseif
-				not target.off and then target.item.date_time >
-				innermost_input.item.date_time
-			then
-				from
-				until
-					target.exhausted or target.item.date_time.is_equal (
-						innermost_input.item.date_time)
-				loop
-					target.back
-				end
-			end
-		ensure
-			dates_equal_if_not_off: not target.exhausted implies
-				target.item.date_time.is_equal (innermost_input.item.date_time)
-		end
+    synchronize_target_with_innermost_input
+            -- Synchronize `target' with `innermost_input'.
+        require
+            innermost_input_not_off: not innermost_input.off
+        do
+            if target.off then
+                target.start
+            end
+            if
+                not target.off and then target.item.date_time <
+                innermost_input.item.date_time
+            then
+                from
+                until
+                    target.exhausted or target.item.date_time.is_equal (
+                        innermost_input.item.date_time)
+                loop
+                    target.forth
+                end
+            elseif
+                not target.off and then target.item.date_time >
+                innermost_input.item.date_time
+            then
+                from
+                until
+                    target.exhausted or target.item.date_time.is_equal (
+                        innermost_input.item.date_time)
+                loop
+                    target.back
+                end
+            end
+        ensure
+            dates_equal_if_not_off: not target.exhausted implies
+                target.item.date_time.is_equal (innermost_input.item.date_time)
+        end
 
 invariant
 
-	target_is_output: target = input.output
+    target_is_output: target = input.output
 
 end -- class FUNCTION_BASED_COMMAND

src/library/event_generation/function_analysis/one_variable_function_analyzer.e
@@ -34,6 +34,7 @@ feature -- Initialization
         do
             set_input(in)
             create start_date_time.make_now
+            create end_date_time.make_now
             operator := op
             -- EVENT_TYPE instances have a one-to-one correspondence to
             -- FUNTION_ANALYZER instances.  Thus this is the appropriate
@@ -43,11 +44,12 @@ feature -- Initialization
             period_type := per_type
         ensure
             set: input = in and operator = op and start_date_time /= Void and
-                event_type /= Void and event_type = ev_type and
-                signal_type = sig_type
+                end_date_time /= Void and event_type /= Void and
+                event_type = ev_type and signal_type = sig_type
             period_type_set: period_type = per_type
             left_offset_0: left_offset = 0
             -- start_date_set_to_now: start_date_time is set to current time
+            -- end_date_set_to_now: end_date_time is set to current time
         end
 
 feature -- Access
@@ -161,7 +163,7 @@ invariant
 
     operator_not_void: operator /= Void
     input_not_void: input /= Void
-    date_not_void: start_date_time /= Void
+    dates_not_void: start_date_time /= Void and end_date_time /= Void
     offset_not_negative: left_offset >= 0
 
 end -- class ONE_VARIABLE_FUNCTION_ANALYZER

src/library/event_generation/function_analysis/two_variable_function_analyzer.e
@@ -20,7 +20,7 @@ class TWO_VARIABLE_FUNCTION_ANALYZER inherit
 
     TWO_VARIABLE_LINEAR_ANALYZER
         redefine
-            start, action
+            start, action, exhausted
         end
 
     MATH_CONSTANTS
@@ -42,6 +42,7 @@ feature -- Initialization
             set_input1 (in1)
             set_input2 (in2)
             create start_date_time.make_now
+            create end_date_time.make_now
             -- EVENT_TYPE instances have a one-to-one correspondence to
             -- FUNTION_ANALYZER instances.  Thus this is the appropriate
             -- place to create this new EVENT_TYPE instance.
@@ -57,6 +58,7 @@ feature -- Initialization
             both_directions: above_to_below and below_to_above
             use_right: use_right_function
             -- start_date_set_to_now: start_date_time is set to current time
+            -- end_date_set_to_now: end_date_time is set to current time
         end
 
 feature -- Access
@@ -176,6 +178,11 @@ feature -- Basic operations
         end
 
 feature {NONE} -- Hook routine implementation
+
+    exhausted: BOOLEAN
+        do
+			Result := Precursor or else target1.item.date_time > end_date_time
+        end
 
     start
         do
@@ -336,7 +343,7 @@ feature -- Implementation
 invariant
 
     input_not_void: input1 /= Void and input1 /= Void
-    date_not_void: start_date_time /= Void
+    dates_not_void: start_date_time /= Void and end_date_time /= Void
     above_or_below: below_to_above or above_to_below
     left_xor_right_function: use_left_function xor use_right_function
 

jjttcc/mas-client

commit f4d86ad9db00c44fe656afe419170dea95642d56

chnaged test due to MAS end-date enforcement
ParameterTest.test_analyzer_parameters_modification2: One component of
this test started failing after commit
549617d12862b60d0ae611c9adcfee6673a9ce47 of the MAS server, which adds
an early implementation of enforcing end-dates for analysis. The
result is that the end dates specified in this test are actually used
by the server now, as opposed to before, when only the start date was
used and the analysis simply ended for a stock/tradable when the
last/latest record was encountered.  This difference caused different
results from previous test runs, which caused the one component to
"fail".  "Fixed" by changing the end date to the year 2038.

test/parameter_test.rb
@@ -3,6 +3,9 @@ require 'minitest/autorun'
 require_relative './test_setup'
 
 
+# NOTE: Some of these tests are unstable because the source (stock) data
+# is not "locked down" - i.e., it could change and thus change the test
+# results as a consequence.
 class ParameterTest < MiniTest::Test
   include TestSetup
   include ParameterTestTools
@@ -434,6 +437,7 @@ class ParameterTest < MiniTest::Test
     end
   end
 
+  # (Note: This test is unstable. See note at top [above class ParameterTest]).
   def test_analyzer_parameters_modification2
     symbol = 'ibm'
     # (Log 2 clients in, set different parameter-settings for the same
@@ -457,7 +461,7 @@ class ParameterTest < MiniTest::Test
     ana_name = 'Slope of MACD Signal Line Cross Above 0 (Buy)'
     slope_macd_sl0_idx = 5
     start_date = DateTime.new(2012, 01, 01)
-    end_date = DateTime.new(2014, 07, 01)
+    end_date = DateTime.new(2038, 07, 01)
     # Note the "- 1", due to the MAS interface using indexes starting at 1:
     selected = [analyzers2[slope_macd_sl0_idx - 1]]
     assert selected[0].name == ana_name, 'correct analyzer'
@@ -471,7 +475,8 @@ class ParameterTest < MiniTest::Test
       cl1data = $client1.analysis_data
       assert cl1data != cl2data, 'cl1/cl2 data should differ'
       assert cl1data[0] != cl2data[0], 'cl1[0]/cl2[0] data should differ'
-      assert cl1data.count != cl2data.count, 'cl1/cl2 counts should differ'
+      assert cl1data.count != cl2data.count, 'cl1/cl2 counts should differ' +
+        "(#{cl1data.count} vs #{cl2data.count})"
       # Test that client2's parameters don't step on client1's parameters.
       do_analysis(symbol, period_type, selected, start_date, end_date, $client1)
       cl1data_II = $client1.analysis_data
@@ -479,9 +484,11 @@ class ParameterTest < MiniTest::Test
       cl2data_II = $client2.analysis_data
       assert cl1data_II != cl2data_II, 'cl1-later vs cl2-later differ'
       assert cl2data_II[0].datetime != cl1data_II[0].datetime,
-        'first event not equal'
+        'first event date not equal'
+      cl2data_II[-1]
       assert cl2data_II[-1].datetime != cl1data_II[-1].datetime,
-        'last event not equal'
+        "last event date not equal " +
+        "[#{cl2data_II[-1].datetime} vs #{cl1data_II[-1].datetime}]"
       assert cl1data.count == cl1data_II.count, 'cl1 - consitent counts'
       assert cl2data_II.count != cl1data_II.count, 'cl1 vs cl2 - counts differ'
       assert cl2data.count == cl2data_II.count, 'cl2 - consitent counts'